57946a9681b2998bb46b8c8e97cd34cf2a34baa8
Mauricio <mauri.f.mores@gmail.com>
2023-02-02 03:31:06 -0300

    web two commit

---
+++ src/main/resources/static/web/js/account.js
@@ -0,0 +1,22 @@
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            data:{},
+        }
+    },
+    created(){
+        this.loadData();
+    },
+    methods:{
+        loadData:function(){
+            axios.get("http://localhost:8080/api/clients")
+            .then(response=>{
+                console.log(response.data[0]);
+                this.data=response.data[0];
+            })
+            .catch(err=>console.log(err));
+            },
+    }
+}).mount('#app');
