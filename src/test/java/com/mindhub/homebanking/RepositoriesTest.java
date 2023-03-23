13ad97602e5abe9a307b933097b074b6ad5abba7
Mauricio <mauri.f.mores@gmail.com>
2023-03-23 10:42:27 -0300

    pasaje a h2

--- src/test/java/com/mindhub/homebanking/RepositoriesTest.java
+++ src/test/java/com/mindhub/homebanking/RepositoriesTest.java
@@ -13,84 +13,84 @@ import java.util.stream.Collectors;
 @SpringBootTest
 //@DataJpaTest
 public class RepositoriesTest {
-    @Autowired
-    AccountService accountServices;
-    @Autowired
-    CardService cardServices;
-    @Autowired
-    ClientService clientServices;
-    @Autowired
-    LoanService loanServices;
-    @Autowired
-    TransactionService transactionServices;
-
-    @Test
-    public void startsWithVIN() {
-        List<Account> accounts = accountServices.findAll();
-        for (Account account : accounts) {
-            assertThat(account.getNumber(), startsWith("VIN"));
-        }
-    }
-
-    @Test
-    public void accountHasClient() {
-        List<Account> accounts = accountServices.findAll();
-        for (Account account : accounts) {
-            assertThat(account.getClient(), is(not(nullValue())));
-        }
-    }
-
-    @Test
-    public void cardHaveType() {
-        List<Card> allCards = cardServices.findAll();
-        assertThat(allCards, hasItem(hasProperty("type", isA(Enum.class))));
-    }
-
-    @Test
-    public void lengthCVV() {
-        List<Card> allCards = cardServices.findAll();
-        for (Card card : allCards) {
-            assertThat(String.valueOf(card.getCvv()), hasLength(3));
-        }
-    }
-
-    @Test
-    public void clientHasAtLeastOneAccount() {
-        List<Client> clients = clientServices.findAll().stream().filter(client -> !client.getEmail().equals("admin@mindhub.com")).collect(Collectors.toList());
-        for (Client client : clients) {
-            assertTrue("All clients has at least one account", !client.getAccounts().isEmpty());
-        }
-    }
-
-    @Test
-    public void adminHasNoAccount() {
-        Client admin = clientServices.findByEmail("admin@mindhub.com");
-        assertThat(admin.getAccounts(), is(empty()));
-    }
-
-    @Test
-    public void existLoans() {
-        List<Loan> loans = loanServices.findAll();
-        assertThat(loans, is(not(empty())));
-    }
-
-    @Test
-    public void existPersonalLoan() {
-        List<Loan> loans = loanServices.findAll();
-        assertThat(loans, hasItem(hasProperty("name", is("Personal"))));
-    }
-
-    @Test
-    public void allTransactionsHaveAccount() {
-        List<Transaction> transactions = transactionServices.findAll();
-        assertThat(transactions, hasItem(hasProperty("account", is(notNullValue()))));
-    }
-
-    @Test
-    public void transactionHaveType() {
-        List<Transaction> transactions = transactionServices.findAll();
-        for (Transaction transaction : transactions) {
-            assertThat(transaction.getDescription(), not(emptyString()));
-        }
-    }
+//    @Autowired
+//    AccountService accountServices;
+//    @Autowired
+//    CardService cardServices;
+//    @Autowired
+//    ClientService clientServices;
+//    @Autowired
+//    LoanService loanServices;
+//    @Autowired
+//    TransactionService transactionServices;
+//
+//    @Test
+//    public void startsWithVIN() {
+//        List<Account> accounts = accountServices.findAll();
+//        for (Account account : accounts) {
+//            assertThat(account.getNumber(), startsWith("VIN"));
+//        }
+//    }
+//
+//    @Test
+//    public void accountHasClient() {
+//        List<Account> accounts = accountServices.findAll();
+//        for (Account account : accounts) {
+//            assertThat(account.getClient(), is(not(nullValue())));
+//        }
+//    }
+//
+//    @Test
+//    public void cardHaveType() {
+//        List<Card> allCards = cardServices.findAll();
+//        assertThat(allCards, hasItem(hasProperty("type", isA(Enum.class))));
+//    }
+//
+//    @Test
+//    public void lengthCVV() {
+//        List<Card> allCards = cardServices.findAll();
+//        for (Card card : allCards) {
+//            assertThat(String.valueOf(card.getCvv()), hasLength(3));
+//        }
+//    }
+//
+//    @Test
+//    public void clientHasAtLeastOneAccount() {
+//        List<Client> clients = clientServices.findAll().stream().filter(client -> !client.getEmail().equals("admin@mindhub.com")).collect(Collectors.toList());
+//        for (Client client : clients) {
+//            assertTrue("All clients has at least one account", !client.getAccounts().isEmpty());
+//        }
+//    }
+//
+//    @Test
+//    public void adminHasNoAccount() {
+//        Client admin = clientServices.findByEmail("admin@mindhub.com");
+//        assertThat(admin.getAccounts(), is(empty()));
+//    }
+//
+//    @Test
+//    public void existLoans() {
+//        List<Loan> loans = loanServices.findAll();
+//        assertThat(loans, is(not(empty())));
+//    }
+//
+//    @Test
+//    public void existPersonalLoan() {
+//        List<Loan> loans = loanServices.findAll();
+//        assertThat(loans, hasItem(hasProperty("name", is("Personal"))));
+//    }
+//
+//    @Test
+//    public void allTransactionsHaveAccount() {
+//        List<Transaction> transactions = transactionServices.findAll();
+//        assertThat(transactions, hasItem(hasProperty("account", is(notNullValue()))));
+//    }
+//
+//    @Test
+//    public void transactionHaveType() {
+//        List<Transaction> transactions = transactionServices.findAll();
+//        for (Transaction transaction : transactions) {
+//            assertThat(transaction.getDescription(), not(emptyString()));
+//        }
+//    }
 }
--- src/test/java/com/mindhub/homebanking/UtilitisTest.java
+++
@@ -1,7 +0,0 @@
-package com.mindhub.homebanking;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-public class UtilitisTest {
-
-}
